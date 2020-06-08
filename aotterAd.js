e520f71c458101c13ac5d54459f61409efe0fcb5
Terence <terence223@mac.com>
2020-06-08 22:13:49 +0800

    fetch data with type

--- aotterAd.js
+++ aotterAd.js
@@ -1,12 +1,21 @@
 (function(window, document, jsonpCallbackName) {
     const API_URL = 'http://localhost:3000/ads/jsonp';
 
-    const scriptEle = document.createElement('script');
-    scriptEle.src = API_URL;
-    document.body.appendChild(scriptEle);
+    class Aotter {
+        loadAd(type = '') {
+            const scriptEle = document.createElement('script');
+            scriptEle.src = `${API_URL}?type=${type}`;
+            document.body.appendChild(scriptEle);
+        }
+    }
+
+    
 
     window[jsonpCallbackName] = jsonpCallback;
     function jsonpCallback(res) {
         console.log('result', res);
     }
+
+    window.aotterAd = new Aotter();
+
 })(window, document, 'jsonpCallback')
