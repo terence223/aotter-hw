1339b3d0355d00881ea71fa3ded1c84c3cbd128d
Terence <terence223@mac.com>
2020-06-09 00:30:17 +0800

    loaded and failed callback

--- aotterAd.js
+++ aotterAd.js
@@ -2,20 +2,50 @@
     const API_URL = 'http://localhost:3000/ads/jsonp';
 
     class Aotter {
+        eventList = {}
+        
+        waitingForEmit = {
+            "on-ad-loaded": false,
+            "on-ad-failed": false,
+            "on-ad-impression": false
+        }
+
         loadAd(type = '') {
             const scriptEle = document.createElement('script');
             scriptEle.src = `${API_URL}?type=${type}`;
             document.body.appendChild(scriptEle);
         }
+
+        on(eventName, cb) {
+            if(this.waitingForEmit[eventName]) {
+                cb();
+                this.waitingForEmit[eventName] = false;
+            }
+            else {
+                this.eventList[eventName] = cb
+            }
+            
+        }
+
+        emit(eventName) {
+            if(this.eventList[eventName]) {
+                this.eventList[eventName]();
+            }
+            else {
+                this.waitingForEmit[eventName] = true;
+            }
+        }
     }
 
     
-
+    
     window[jsonpCallbackName] = jsonpCallback;
+
     function jsonpCallback(res) {
         console.log('result', res);
         let ad;
         if(!res.success) {
+            aotterAd.emit('on-ad-failed');
             return;
         }
 
@@ -32,6 +62,7 @@
         }
     
         document.getElementById('aotter-ad-plugin').appendChild(ad);
+        aotterAd.emit('on-ad-loaded');
     }
 
     window.aotterAd = new Aotter();
